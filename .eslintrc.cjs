3a0b337fb798e38028650537adb88a8d439d2756
Alin Florin <alin@qoffice.ro>
2023-06-09 13:23:13 +0300

    -

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,16 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-var-requires': 0,
   },
 }
