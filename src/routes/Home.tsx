74d366a342fcf72687120c3a67d2bf15756962a9
Alin Florin <alin@qoffice.ro>
2023-06-29 00:25:22 +0300

    -

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,8 @@
-import { Button } from '@fluentui/react-components';
-import { useCallback } from 'react';
-import useUserPreferencesService from '../services/user-preferences.service';
-
 export const Home = () => {
-  const { getUserPreferences } = useUserPreferencesService();
-  const onUserPreferencesClick = useCallback(async () => {
-    const prefs = await getUserPreferences();
-    console.log(prefs);
-  }, [getUserPreferences]);
 
   return (
     <div test-id="hello">
       hello aolo home <br />
-      <Button onClick={onUserPreferencesClick}>test get user prefs</Button>
     </div>
   );
 };
