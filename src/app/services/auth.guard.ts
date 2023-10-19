7b6e3e230ecff3abab3aaabe1722fd1c83e5c195
Alin Florin <alin@qoffice.ro>
2023-10-20 00:08:29 +0300

    -

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,16 +1,18 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { tap } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const oidcSecurityService = inject(OidcSecurityService);
-  return oidcSecurityService.isAuthenticated().pipe(
-    tap(isAuth => {
-      if (!isAuth) {
+  return oidcSecurityService.isAuthenticated$.pipe(
+    tap(ar => {
+      console.log(ar);
+      if (!ar.isAuthenticated) {
         router.navigateByUrl('/login?returnTo=' + encodeURIComponent(state.url))
       }
-    })
+    }),
+    map(x => x.isAuthenticated)
   );
 };
