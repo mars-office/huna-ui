6922992d9ba8051517404ef82fc1479b534707d7
Alin Florin <alin@qoffice.ro>
2023-11-18 00:10:49 +0200

    asd

--- intercept.sh
+++ intercept.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 telepresence connect -n huna
-telepresence intercept huna-huna-ui --port 3000:http --env-file ./.env || true
+telepresence intercept huna-huna-ui --port 3000:http --to-pod 8181 --env-file ./.env || true
