8cd50a4307709febbab1762b50c5cd895db51337
Alin Florin <alin@qoffice.ro>
2023-11-27 20:17:18 +0200

    asd

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,17 +1,37 @@
 import Header from './layout/Header';
 import Footer from './layout/Footer';
-import { useAuth } from 'react-oidc-context';
+import { hasAuthParams, useAuth } from 'react-oidc-context';
 import { Routing } from './Routing';
 import { useStore } from './hooks/use-store';
 import { userStore } from './stores/user-store';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Layout = () => {
   const auth = useAuth();
+  const [hasTriedSignin, setHasTriedSignin] = useState(false);
   
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setUserStoreUser] = useStore(userStore);
   
+  useEffect(() => {
+    if (!hasAuthParams() && auth.user != null &&
+        !auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading &&
+        !hasTriedSignin
+    ) {
+        setHasTriedSignin(true);
+        (async () => {
+          try {
+            const newUser = await auth.signinSilent();
+            if (!newUser) {
+              await auth.signoutRedirect();
+            }
+          } catch (err) {
+            console.error(err);
+            await auth.signoutRedirect();
+          }
+        })();
+    }
+}, [auth, hasTriedSignin, auth.user]);
 
   useEffect(() => {
     setUserStoreUser(!auth.user ? undefined : auth.user);
