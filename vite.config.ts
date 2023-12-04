058b55b641715c28fdeac4dec917f22f2508f07b
Alin Florin <alin@qoffice.ro>
2023-12-05 00:46:12 +0200

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,52 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf,otf,woff,woff2,jsx,tsx,ts}']
+      },
+      manifest: {
+        name: 'huna2',
+        short_name: 'huna',
+        description: 'huna2',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    }),
+  ],
   server: {
     port: 3000,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
   build: {
-    chunkSizeWarningLimit: 2048
+    chunkSizeWarningLimit: 2048,
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["./tests/setup.ts"],
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
   },
 });
