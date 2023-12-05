736f8c53429f4cd725374ab00625a477036f2f4a
Alin Florin <alin@qoffice.ro>
2023-12-05 16:35:39 +0200

    -

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       registerType: 'prompt',
       injectRegister: 'auto',
       workbox: {
-        globPatterns: ['**/*.{js,mjs,css,html,ico,png,svg,ttf,otf,woff,woff2,jsx,tsx,ts}']
+        globPatterns: ['**/*.{js,mjs,css,html,ico,png,svg,ttf,otf,woff,woff2,jsx,tsx,ts,json,dll,jpg,jpeg,gif,bmp,webp,webm,tiff,avi,mp4,mp3,aac}']
       },
       manifest: {
         name: 'huna2',
