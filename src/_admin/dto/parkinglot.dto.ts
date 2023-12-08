b8f9411ebadc5ea2584af3e1bbe4f126b064798f
Alin Florin <alin@qoffice.ro>
2023-12-08 22:20:30 +0200

    -

--- src/_admin/dto/parkinglot.dto.ts
+++ src/_admin/dto/parkinglot.dto.ts
@@ -1,5 +1,5 @@
 export interface ParkingLotDto {
-  _id: string;
+  _id?: string;
   name: string;
   lat: number;
   lng: number;
--- src/_admin/routes/ParkingLots.tsx
+++ src/_admin/routes/ParkingLots.tsx
@@ -1,48 +1,70 @@
 import { Body1, Button, Card, CardFooter, CardHeader, Text } from '@fluentui/react-components';
 import {
+  AddRegular,
   ArrowDownloadRegular,
+  ArrowRepeatAllRegular,
   ArrowSyncRegular,
   DeleteRegular,
   EditRegular,
 } from '@fluentui/react-icons';
 import { useTranslation } from 'react-i18next';
 import ConfirmationButton from '../../components/ConfirmationButton';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { ParkingLotDto } from '../dto/parkinglot.dto';
 import parkingLotsService from '../services/parkinglots.service';
 import { useToast } from '../../hooks/use-toast';
 import zipService from '../../services/zip.service';
 import downloadService from '../../services/download.service';
+import AddEditParkingLotDialog from './AddEditParkingLotDialog';
+import Loading from '../../layout/Loading';
 
 export const ParkingLots = () => {
   const { t } = useTranslation();
   const [parkingLots, setParkingLots] = useState<ParkingLotDto[]>([]);
   const { fromError, toast } = useToast();
+  const [addEditOpen, setAddEditOpen] = useState(false);
+  const [editedItem, setEditedItem] = useState<ParkingLotDto | undefined>(undefined);
+  const [loading, setLoading] = useState(false);
+  const parkingLotsListDiv = useRef<HTMLDivElement | undefined>(undefined);
+
+  const loadParkingLots = useCallback(async () => {
+    try {
+      setLoading(true);
+      const lots = await parkingLotsService.getParkingLots();
+      setParkingLots(lots);
+      setLoading(false);
+    } catch (err) {
+      fromError(err);
+      setLoading(false);
+    }
+  }, [setLoading, fromError, setParkingLots]);
 
   useEffect(() => {
     (async () => {
-      try {
-        const lots = await parkingLotsService.getParkingLots();
-        setParkingLots(lots);
-      } catch (err) {
-        fromError(err);
-      }
+      await loadParkingLots();
     })();
   }, []);
 
-  const downloadCertificates = useCallback(async (_id: string) => {
-    try {
-      const result = await parkingLotsService.getCertificate(_id);
-      const zipBlob = await zipService.zipFiles({
-        'ca.crt': result.caCrt,
-        'client.crt': result.clientCertificateCrt,
-        'client.key': result.clientCertificateKey
-      });
-      downloadService.downloadBlob(zipBlob, _id + '_certificate.zip');
-    } catch (err) {
-      fromError(err);
-    }
-  }, [fromError]);
+  useEffect(() => {
+    parkingLotsListDiv.current?.scrollTo({ top: 0 });
+  }, [parkingLots]);
+
+  const downloadCertificates = useCallback(
+    async (_id: string) => {
+      try {
+        const result = await parkingLotsService.getCertificate(_id);
+        const zipBlob = await zipService.zipFiles({
+          'ca.crt': result.caCrt,
+          'client.crt': result.clientCertificateCrt,
+          'client.key': result.clientCertificateKey,
+        });
+        downloadService.downloadBlob(zipBlob, _id + '_certificate.zip');
+      } catch (err) {
+        fromError(err);
+      }
+    },
+    [fromError],
+  );
 
   const deleteParkingLot = useCallback(
     async (_id: string, i: number) => {
@@ -71,59 +93,123 @@ export const ParkingLots = () => {
     [t, fromError, toast],
   );
 
+  const editClick = useCallback(
+    (dto: ParkingLotDto) => {
+      setEditedItem(dto);
+      setAddEditOpen(true);
+    },
+    [setEditedItem, setAddEditOpen],
+  );
+
+  const dialogOpenChanged = useCallback(
+    (o: boolean) => {
+      setAddEditOpen(o);
+      if (!o) {
+        setTimeout(() => {
+          setEditedItem(undefined);
+        }, 300);
+      }
+    },
+    [setEditedItem, setAddEditOpen],
+  );
+
+  const parkingLotAdded = useCallback(
+    (dto: ParkingLotDto) => {
+      setParkingLots((s) => [...s, dto].sort((a, b) => a.name.localeCompare(b.name)));
+      toast('success', t('ui.admin.parkingLots.addedSuccessfully'));
+    },
+    [toast, t, setParkingLots],
+  );
+
+  const parkingLotUpdated = useCallback(
+    (dto: ParkingLotDto) => {
+      setParkingLots((s) => {
+        const list = [...s];
+        list[list.findIndex((z) => z._id === dto._id)] = dto;
+        return list.sort((a, b) => a.name.localeCompare(b.name));
+      });
+      toast('success', t('ui.admin.parkingLots.editedSuccessfully'));
+    },
+    [toast, t, setParkingLots],
+  );
+
   return (
-    <div style={{ display: 'flex', height: '100%', flexDirection: 'column', gap: '1rem' }}>
-      <Text size={500}>{t('ui.admin.parkingLots.parkingLots')}</Text>
-      <div
-        style={{
-          flex: '1 1 auto',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1rem',
-          minHeight: '0px',
-          overflow: 'auto',
-        }}
-      >
-        {parkingLots.map((pl, i) => (
-          <Card key={pl._id} appearance="filled-alternative">
-            <CardHeader
-              header={
-                <Body1>
-                  <b>{pl.name}</b>
-                </Body1>
-              }
-              description={
-                <small>
-                  {pl._id}, {pl.lat} - {pl.lng}
-                </small>
-              }
-            ></CardHeader>
-            <CardFooter
-              action={
-                <>
-                  <Button
-                    onClick={() => downloadCertificates(pl._id)}
-                    appearance="subtle"
-                    icon={<ArrowDownloadRegular />}
-                  ></Button>
-                  <ConfirmationButton
-                    appearance="subtle"
-                    onClick={() => regenerateCertificate(pl._id)}
-                    icon={<ArrowSyncRegular />}
-                  ></ConfirmationButton>
-                  <Button appearance="subtle" icon={<EditRegular />}></Button>
-                  <ConfirmationButton
-                    onClick={() => deleteParkingLot(pl._id, i)}
-                    appearance="subtle"
-                    icon={<DeleteRegular />}
-                  ></ConfirmationButton>
-                </>
-              }
-            ></CardFooter>
-          </Card>
-        ))}
+    <>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        <Text size={500}>{t('ui.admin.parkingLots.parkingLots')}</Text>
+        <div
+          ref={parkingLotsListDiv}
+          style={{
+            flex: 'auto',
+            minHeight: '0px',
+            overflow: 'auto',
+          }}
+        >
+          {loading && <Loading />}
+          {!loading && (!parkingLots || parkingLots.length === 0) && (
+            <div>{t('ui.admin.parkingLots.noItems')}</div>
+          )}
+          {!loading &&
+            parkingLots.map((pl, i) => (
+              <Card
+                key={pl._id}
+                style={{ marginBottom: i < parkingLots.length - 1 ? '1rem' : '0rem' }}
+                appearance="filled-alternative"
+              >
+                <CardHeader
+                  header={
+                    <Body1>
+                      <b>{pl.name}</b>
+                    </Body1>
+                  }
+                  description={
+                    <small>
+                      {pl._id}, {pl.lat} - {pl.lng}
+                    </small>
+                  }
+                ></CardHeader>
+                <CardFooter
+                  action={
+                    <>
+                      <Button
+                        onClick={() => downloadCertificates(pl._id!)}
+                        appearance="subtle"
+                        icon={<ArrowDownloadRegular />}
+                      ></Button>
+                      <ConfirmationButton
+                        appearance="subtle"
+                        onClick={() => regenerateCertificate(pl._id!)}
+                        icon={<ArrowSyncRegular />}
+                      ></ConfirmationButton>
+                      <Button
+                        appearance="subtle"
+                        onClick={() => editClick(pl)}
+                        icon={<EditRegular />}
+                      ></Button>
+                      <ConfirmationButton
+                        onClick={() => deleteParkingLot(pl._id!, i)}
+                        appearance="subtle"
+                        icon={<DeleteRegular />}
+                      ></ConfirmationButton>
+                    </>
+                  }
+                ></CardFooter>
+              </Card>
+            ))}
+        </div>
+        <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+          <Button onClick={() => setAddEditOpen(true)} icon={<AddRegular />}></Button>
+          <Button onClick={loadParkingLots} icon={<ArrowRepeatAllRegular />}></Button>
+        </div>
       </div>
-    </div>
+      <AddEditParkingLotDialog
+        itemAdded={parkingLotAdded}
+        itemUpdated={parkingLotUpdated}
+        onOpenChange={(o) => dialogOpenChanged(o)}
+        open={addEditOpen}
+        editedItem={editedItem}
+      />
+    </>
   );
 };
 
