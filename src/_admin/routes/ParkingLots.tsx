31056e504607f4a126855eb0e2276acd24ba6195
Alin Florin <alin@qoffice.ro>
2023-12-08 14:10:48 +0200

    -

--- src/_admin/routes/ParkingLots.tsx
+++ src/_admin/routes/ParkingLots.tsx
@@ -1,12 +1,33 @@
-import { Text } from "@fluentui/react-components";
-import { useTranslation } from "react-i18next";
+import {
+  Button,
+  Card,
+  CardFooter,
+  CardHeader,
+  CardPreview,
+  Text,
+} from '@fluentui/react-components';
+import { DeleteRegular,  EditRegular } from '@fluentui/react-icons';
+import { useTranslation } from 'react-i18next';
+import ConfirmationButton from '../../components/ConfirmationButton';
 
 export const ParkingLots = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
-  return <>
-    <Text size={500}>{t('ui.admin.parkingLots.parkingLots')}</Text>
-  </>
-}
+  return (
+    <div style={{ display: 'flex', height: '100%', flexDirection: 'column', gap: '1rem' }}>
+      <Text size={500}>{t('ui.admin.parkingLots.parkingLots')}</Text>
+      <div style={{ flex: '1 1 auto', minHeight: '0px', overflow: 'auto' }}>
+        <Card>
+          <CardHeader header={'test'} description={'lat lng'}></CardHeader>
+          <CardPreview>asdasd</CardPreview>
+          <CardFooter>
+            <Button appearance='subtle' icon={<EditRegular />}>{t('ui.admin.parkingLots.edit')}</Button>
+            <ConfirmationButton onClick={() => alert(1)} appearance='subtle' icon={<DeleteRegular />}>{t('ui.admin.parkingLots.delete')}</ConfirmationButton>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default ParkingLots;
---
+++ src/components/ConfirmationButton.tsx
@@ -0,0 +1,46 @@
+import { Button, ButtonProps } from '@fluentui/react-components';
+import { QuestionCircleRegular } from '@fluentui/react-icons';
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const ConfirmationButton = (props: ButtonProps) => {
+  const { t } = useTranslation();
+  const [clicked, setClicked] = useState(false);
+
+  const onFocus = useCallback(() => {
+    if (!clicked) {
+      setClicked(true);
+      return;
+    }
+  }, [clicked, setClicked]);
+
+  const onBlur = useCallback(() => {
+    if (clicked) {
+      setClicked(false);
+      return;
+    }
+  }, [clicked, setClicked]);
+
+  const onClick = useCallback(
+    (e: any) => {
+      setClicked(false);
+      if (props.onClick) {
+        props.onClick(e);
+      }
+    },
+    [props, setClicked],
+  );
+
+  return (
+    <>
+      {clicked && (
+        <Button {...props} onClick={onClick} autoFocus={true} onBlur={onBlur} icon={<QuestionCircleRegular />}>
+          {t('ui.components.confirmationButton.areYouSure')}
+        </Button>
+      )}
+      {!clicked && <Button {...props} onFocus={onFocus} />}
+    </>
+  );
+};
+
+export default ConfirmationButton;
