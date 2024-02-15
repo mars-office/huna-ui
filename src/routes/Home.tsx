e6aa2d9257b9bf2b33f33790f9b299742de965b5
Alin Florin <alin@qoffice.ro>
2024-02-15 22:24:14 +0200

    d

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,14 @@
 import { HomeFilled } from "@fluentui/react-icons";
 import ConfirmationButton from "../components/ConfirmationButton";
+import { useSignalrData } from "../hooks/use-signalr-data";
+import { useToast } from "../hooks/use-toast";
 
 export const Home = () => {
-
+  const {toast} = useToast();
+  useSignalrData(async (d: any) => {
+    console.log(d);
+    toast("info", d.message);
+  }, [toast]);
   return (
     <div test-id="hello">
       hello aolo home ????<br /> <ConfirmationButton onClick={() => alert(1)} icon={<HomeFilled />}></ConfirmationButton>
