29977cc07aad6a10fb8b74cc90acb7f0214c3e1a
Alin Florin <alin@qoffice.ro>
2024-05-21 20:24:37 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,mjs,css,html,ico,png,svg,ttf,otf,woff,woff2,jsx,tsx,ts,json,dll,jpg,jpeg,gif,bmp,webp,webm,tiff,avi,mp4,mp3,aac}'],
         navigateFallbackDenylist: [/^\/api/],
-        importScripts: ['/service-worker/push.js'],
+        // importScripts: ['/service-worker/push.js'],
         clientsClaim: true,
         skipWaiting: true
       },
